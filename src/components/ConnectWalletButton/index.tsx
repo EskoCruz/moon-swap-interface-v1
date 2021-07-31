1e48540c93ccdeaf4a9207f2d763e78db303656b
Esko Cruzeta <eskocruz@users.noreply.github.com>
2021-07-31 09:33:49 -0700

    wallet connect

--- src/components/ConnectWalletButton/index.tsx
+++ src/components/ConnectWalletButton/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { useWalletModal } from '@pancakeswap-libs/uikit'
+// import { useWalletModal } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import useAuth from 'hooks/useAuth'
+import { useWalletModal } from '@pancakeswap-libs/uikit'
+// import { useWalletModal } from '../../widgets/WalletModal'
 import { Button, ButtonProps } from '../Button'
 
 const UnlockButton: React.FC<ButtonProps> = (props) => {
