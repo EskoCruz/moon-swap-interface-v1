cbff8f4c608d2b76d1e12e283704df71e663e528
Esko Cruzeta <eskocruz@users.noreply.github.com>
2021-08-23 20:13:19 -0700

    stash

--- src/widgets/WalletModal/types.ts
+++ src/widgets/WalletModal/types.ts
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { SvgProps } from "../../components/Svg";
+import { SvgProps } from "../../components/Svg/types";
 
 export enum ConnectorNames {
   Injected = "injected",
@@ -13,4 +13,5 @@ export interface Config {
   title: string;
   icon: FC<SvgProps>;
   connectorId: ConnectorNames;
+  priority: number;
 }
