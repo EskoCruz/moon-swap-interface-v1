560858a26bedc5991cd260d5c918907c9a05414a
Esko Cruzeta <eskocruz@users.noreply.github.com>
2021-08-26 08:01:51 -0700

    Border fix

--- src/widgets/Modal/styles.tsx
+++ src/widgets/Modal/styles.tsx
@@ -41,11 +41,11 @@ export const ModalBackButton: React.FC<{ onBack: ModalProps["onBack"] }> = ({ on
 
 export const ModalContainer = styled(Box)<{ minWidth: string }>`
   overflow: hidden;
-  // background: ${({ theme }) => theme.modal.background};
-  background: black;
+  background: ${({ theme }) => theme.modal.background};
+  // background: black;
   box-shadow: 0px 20px 36px -8px rgba(14, 14, 44, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
   border: 1px solid ${({ theme }) => theme.colors.borderColor};
-  border-radius: 32px;
+  // border-radius: 32px;
   width: 100%;
   z-index: ${({ theme }) => theme.zIndices.modal};
 
